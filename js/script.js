fdf621742f9e79185653da0aef0dba63ac047b00
arbi <arbih@mail.ru>
2021-11-14 01:57:54 +0300

    first java change

--- js/script.js
+++ js/script.js
@@ -1,2 +1,18 @@
 "use strict";
-console.log();
+const numberoffilms = +prompt("Сколько фильмов вы просмотрели?", "" );
+const personalmovieDB = {
+    count: numberoffilms,
+    movies: {},
+    actors: {},
+    genres:[],
+    privat:false
+};
+const a = prompt("последний просмотренный фильм", ""),
+    b = prompt ("оценка фильму", ""),
+    c = prompt("последний просмотренный фильм", ""),
+    d = prompt ("оценка фильму", "");
+    
+    personalmovieDB.movies [a] = b;
+    personalmovieDB.movies [c] = d;
+    
+    console.log(personalmovieDB);
